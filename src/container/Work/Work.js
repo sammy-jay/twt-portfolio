c16dfb29fae9b1c6a2e3e5c07c5fe24ec4bfbe04
Sammy Jay <68499075+sammy-jay@users.noreply.github.com>
2022-03-19 00:59:46 +0100

    Update Work.js

--- src/container/Work/Work.js
+++ src/container/Work/Work.js
@@ -113,44 +113,24 @@ const Work = () => {
               <p className="p-text" style={{ marginTop: 10 }}>
                 {work.description}
               </p>
-              <div style={{ width:"100%", display:"flex", justifyContent:"center" }}>
+              <div style={{ width:"100%", display:"flex", justifyContent:"space-evenly", marginTop: "5px"}}>
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
 
-                  <motion.div
+                  
+                    <AiFillEye size={28}/>
 
-                    whileInView={{ scale: [0, 1] }}
-
-                    whileHover={{ scale: [1, 0.9] }}
-
-                    transition={{ duration: 0.25 }}
-
-                    className="app__flex"
-
-                  >
-
-                    <AiFillEye />
-
-                  </motion.div>
+                  
 
                 </a>
 
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
 
-                  <motion.div
+                  
+                 
 
-                    whileInView={{ scale: [0, 1] }}
+                    <AiFillGithub size={28}/>
 
-                    whileHover={{ scale: [1, 0.9] }}
-
-                    transition={{ duration: 0.25 }}
-
-                    className="app__flex"
-
-                  >
-
-                    <AiFillGithub />
-
-                  </motion.div>
+                  
 
                 </a>
               </div>
