6e1102024dd9cd60a6d2880d36635151afb0e82c
Sammy Jay <68499075+sammy-jay@users.noreply.github.com>
2022-03-18 18:07:04 +0100

    Update Header.js

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -53,20 +53,20 @@ const Header = () => {
           </div>
         </div>
       </motion.div>
-      <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        <img src={images.profile} alt="profile_bg" />
-        <motion.img
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: "easeInOut" }}
-          src={images.circle}
-          alt="profile_circle"
-          className="overlay_circle"
-        />
-      </motion.div>
+//       <motion.div
+//         whileInView={{ opacity: [0, 1] }}
+//         transition={{ duration: 0.5, delayChildren: 0.5 }}
+//         className="app__header-img"
+//       >
+//         <img src={images.profile} alt="profile_bg" />
+//         <motion.img
+//           whileInView={{ scale: [0, 1] }}
+//           transition={{ duration: 1, ease: "easeInOut" }}
+//           src={images.circle}
+//           alt="profile_circle"
+//           className="overlay_circle"
+//         />
+//       </motion.div>
 
       <motion.div
         variants={scaleVariants}
