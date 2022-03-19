0cba893470ae51ba6f4b5affa63df4cc2c5b7a3f
Sammy Jay <68499075+sammy-jay@users.noreply.github.com>
2022-03-19 22:12:50 +0100

    Update Header.js

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -31,7 +31,7 @@ const Header = () => {
     <div id="home" className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 2 }}
+        transition={{ duration: 1 }}
         className="app__header-info"
       >
         <div className="app__header-badge">
