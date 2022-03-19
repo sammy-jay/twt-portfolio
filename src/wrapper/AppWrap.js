a3355567ca808a6ffb6d9bbc10fe80a56c3fba2c
sammy-jay <sammyjayden27@gmail.com>
2022-03-19 06:54:21 +0100

    2 files modified

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { NavigationDots, SocialMedia } from "../components";
+import React from 'react';
+import { NavigationDots, SocialMedia } from '../components';
 
 const AppWrap = (Component, idName, classNames) =>
   function HOC() {
@@ -10,6 +10,35 @@ const AppWrap = (Component, idName, classNames) =>
           <Component />
 
           <div className="copyright">
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                margin: '4px auto',
+              }}
+            >
+              <div>
+                <a
+                  href="https://linkedin.com/in/tolulope-soneye-5526a51b7"
+                  target="_blank"
+                >
+                  <BsLinkedin />
+                </a>
+              </div>
+              <div>
+                <a
+                  href="https://twitter.com/TolulopeSoneye3?t=3XdYF_fZljuuDMfpYluONw&s=09"
+                  target="_blank"
+                >
+                  <BsTwitter />
+                </a>
+              </div>
+              <div>
+                <a href="" target="_blank">
+                  <BsInstagram />
+                </a>
+              </div>
+            </div>
             <p className="p-text">&copy;2022 TOLULOPE</p>
             <p className="p-text">All rights reserved</p>
           </div>
