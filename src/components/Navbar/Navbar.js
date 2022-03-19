8c29d1277667793ff1f71197b3fc1b52fddfaf29
Sammy Jay <68499075+sammy-jay@users.noreply.github.com>
2022-03-19 20:55:50 +0100

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        <img src="https://res.cloudinary.com/techwithtols/image/upload/v1647719685/portfolio/LogoMakr-79Bf9d_w3hkvc.png" alt="logo" />
       </div>
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
