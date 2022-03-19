e08ededc6aced61d5e895aed0ab8ab007c966147
Sammy Jay <68499075+sammy-jay@users.noreply.github.com>
2022-03-19 07:14:41 +0100

    Update AppWrap.js

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationDots, SocialMedia } from '../components';
-import { BsTwitter, BsInstagram,BsLinkedin } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+
 
 const AppWrap = (Component, idName, classNames) =>
   function HOC() {
@@ -12,30 +11,7 @@ const AppWrap = (Component, idName, classNames) =>
           <Component />
 
           <div className="copyright">
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                margin: '4px auto',
-              }}
-            >
-              <div>
-                <a
-                  href="https://linkedin.com/in/tolulope-soneye-5526a51b7"
-                  target="_blank"
-                >
-                  <BsLinkedin />
-                </a>
-              </div>
-              <div>
-                <a
-                  href="https://twitter.com/TolulopeSoneye3?t=3XdYF_fZljuuDMfpYluONw&s=09"
-                  target="_blank"
-                >
-                  <BsTwitter />
-                </a>
-              </div>
-              <div>
+           
                 <a href="" target="_blank">
                   <BsInstagram />
                 </a>
