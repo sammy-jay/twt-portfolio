ed54790f54d01fa2d2809eddc269e42aca646a59
Sammy Jay <68499075+sammy-jay@users.noreply.github.com>
2022-03-19 21:55:02 +0100

    Update Header.js

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -53,17 +53,20 @@ const Header = () => {
           </div>
         </div>
       </motion.div>
-       <motion.div
-        variants={scaleVariants}
-        whileInView={scaleVariants.whileInView}
-        className="app__header-circles hide-mobile"
-      >
-        {[images.javascript, images.python, images.figma].map((circle, index) => (
-          <div className="circle-cmp app__flex" key={`circle-${index}`}>
-            <img src={circle} alt="profile_bg" />
-          </div>
-        ))}
-      </motion.div>
+      <motion.div
+      whileInView={{ opacity: [0, 1] }}
+      transition={{ duration: 0.5, delayChildren: 0.5 }}
+      className="app__header-img"
+    >
+      <img src="https://res.cloudinary.com/techwithtols/image/upload/c_scale,q_100,r_30,w_560/a_-10/v1647722425/portfolio/profile_zzxajo.jpg" alt="profile_bg" />
+      <motion.img
+        whileInView={{ scale: [0, 1] }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        src={images.circle}
+        alt="profile_circle"
+        className="overlay_circle"
+      />
+    </motion.div>
 
       <motion.div
         variants={scaleVariants}
